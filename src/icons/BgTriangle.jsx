106767262c4c814dbb28381ad0cdc5d2547ec6e0
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-10-25 04:19:03 +0300

    feat(icons): add all project icons & manage its export to one file index

---
+++ src/icons/BgTriangle.jsx
@@ -0,0 +1,12 @@
+const BgTriangle = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={313} height={278} {...props}>
+    <path
+      stroke="#000"
+      strokeWidth={15}
+      fill="none"
+      opacity={0.2}
+      d="M156.5 262 300 8H13z"
+    />
+  </svg>
+);
+export default BgTriangle;
---
+++ src/icons/index.js
@@ -0,0 +1,9 @@
+import BgTriangle from "./BgTriangle";
+import CloseIcon from "./CloseIcon";
+import PaperIcon from "./PaperIcon";
+import RockIcon from "./RockIcon";
+import ScissorsIcon from "./ScissorsIcon";
+import GameRules from "./GameRules";
+import Logo from "./Logo";
+
+export { BgTriangle, CloseIcon, PaperIcon, RockIcon, ScissorsIcon, GameRules, Logo }
