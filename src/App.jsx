ad3621a006c55d7e828faa08737d87989bb89230
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-13 09:07:40 +0200

    style(App): add mobile-first styles for App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import GameShapes from "./components/GameShapes/GameShapes";
 import Footer from "./components/Footer/Footer";
 export default function App() {
   return (
-    <article>
+    <article className="w-full h-full p-4 flex flex-col justify-between items-center">
       <Header />
       <GameShapes />
       <Footer />
