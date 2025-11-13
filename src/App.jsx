94315585c6a5eadeea286999ace13696f091e77a
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-13 06:06:07 +0200

    feat(App): integrate main components inside App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
-export default function App() {}
+import Header from "./components/Header/Header";
+import GameShapes from "./components/GameShapes/GameShapes";
+import Footer from "./components/Footer/Footer";
+export default function App() {
+  return (
+    <article>
+      <Header />
+      <GameShapes />
+      <Footer />
+    </article>
+  );
+}
