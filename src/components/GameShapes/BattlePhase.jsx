f3a15be2f897e93cc16e7f6da3304778e9a73ffb
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-13 03:42:07 +0200

    feat(BattlePhase): add BattlePhase component

---
+++ src/components/GameShapes/BattlePhase.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import useGameData from "../../store/useGameData";
+import GameShapeItem from "./GameShapeItem";
+import ResultStatus from "./ResultStatus";
+
+export default function BattlePhase() {
+  const { playerChoice, computerChoice } = useGameData(
+    (state) => state.battlePhase
+  );
+  const updateShowResult = useGameData((state) => state.updateShowResult);
+  const showResult = useGameData((state) => state.showResult);
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => updateShowResult(), 2000);
+    return () => clearTimeout(timeOutId);
+  }, []);
+  return (
+    <>
+      <section>
+        <GameShapeItem shapeData={playerChoice} isComp={false} />
+        <p>YOU PICKED</p>
+      </section>
+      <section>
+        {showResult ? (
+          <GameShapeItem shapeData={computerChoice} />
+        ) : (
+          <div></div>
+        )}
+        <p>THE HOUSE PICKED</p>
+      </section>
+      {showResult && <ResultStatus />}
+    </>
+  );
+}
