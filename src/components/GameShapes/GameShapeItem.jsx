ab018d2a893b707f0b7c4f6c6c1db54e950bcdaf
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-13 03:23:16 +0200

    feat(GameShapeItem): add GameShapeItem component

---
+++ src/components/GameShapes/GameShapeItem.jsx
@@ -0,0 +1,18 @@
+import useGameData from "../../store/useGameData";
+
+export default function GameShapeBtn({ shapeData }) {
+  const updateBattlePhase = useGameData((state) => state.updateBattlePhase);
+  const battleStarted = useGameData((state) => state.battleStarted);
+
+  return (
+    <section>
+      <button
+        disabled={battleStarted}
+        id={shapeData.id}
+        onClick={(e) => updateBattlePhase(e)}
+        type="button">
+        <shapeData.icon />
+      </button>
+    </section>
+  );
+}
