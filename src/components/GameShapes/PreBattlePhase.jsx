ded7ac346e3c88c16dddcd75406be700485c3cd7
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-13 03:29:00 +0200

    feat(PreBattlePhase): add PreBattlePhase component

---
+++ src/components/GameShapes/PreBattlePhase.jsx
@@ -0,0 +1,17 @@
+import useGameData from "../../store/useGameData";
+import GameShapeItem from "./GameShapeItem";
+import { BgTriangle } from "../../icons";
+
+export default function PreBattlePhase() {
+  const shapesData = useGameData((state) => state.shapes);
+  return (
+    <>
+      <div>
+        <BgTriangle />
+      </div>
+      {shapesData.map((shape) => (
+        <GameShapeItem key={shape.id} shapeData={shape} isComp={false} />
+      ))}
+    </>
+  );
+}
