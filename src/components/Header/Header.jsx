1a22c832a32ec647521ec0bd86f8f87fdfc97746
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-14 09:56:33 +0200

    style(Header): add mobile-first styles for Header component & its subcomponent

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ import Score from "./Score";
 
 export default function Header() {
   return (
-    <header>
-      <Logo />
+    <header className="w-full p-[0.7rem] flex justify-between items-center border-3 border-gray-600 rounded-lg ">
+      <Logo className="w-[285px] h-[70px]" />
       <Score />
     </header>
   );
--- src/components/Header/Score.jsx
+++ src/components/Header/Score.jsx
@@ -4,9 +4,9 @@ export default function Score() {
   const score = useGameData((state) => state.score);
 
   return (
-    <section>
-      <h1>SCORE</h1>
-      <p>{score}</p>
+    <section className="py-2 px-[1.7rem] leading-[1.3] rounded-lg bg-[#fbfafa] flex flex-col justify-end items-center aspect-square ">
+      <h1 className="text-blue-700 text-[0.7rem] font-bold">SCORE</h1>
+      <p className="text-navy-900 text-[2.5rem] font-bold">{score}</p>
     </section>
   );
 }
