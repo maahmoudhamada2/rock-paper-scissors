4bf96744fb5dc7d59e5e2c9ea5cceed4e8e1b562
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-15 07:35:05 +0200

    style(GameShapeItem): add mobile-first styles for GameShapeItem component

--- src/components/GameShapes/GameShapeItem.jsx
+++ src/components/GameShapes/GameShapeItem.jsx
@@ -4,9 +4,19 @@ export default function GameShapeItem({ shapeData }) {
   const updateBattlePhase = useGameData((state) => state.updateBattlePhase);
   const battleStarted = useGameData((state) => state.battleStarted);
 
+  const borderColors = {
+    paper: "border-blue-500",
+    scissors: "border-gold-500",
+    rock: "border-red-600",
+  };
+
   return (
-    <section>
+    <section
+      className={`z-1 relative w-32 rounded-[50%] aspect-square bg-white border-18 ${
+        borderColors[shapeData.name]
+      } inset-shadow-choose-btn primary-btn-shadow`}>
       <button
+        className=" w-full h-full flex items-center justify-center"
         disabled={battleStarted}
         id={shapeData.id}
         onClick={(e) => updateBattlePhase(e)}
