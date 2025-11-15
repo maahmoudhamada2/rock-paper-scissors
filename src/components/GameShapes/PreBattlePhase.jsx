42850b96ea76324e7acac14c52941793a35c11e8
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-15 06:18:55 +0200

    feat(PreBattlePhase): add mobile-first style for PreBattlePhase component

--- src/components/GameShapes/PreBattlePhase.jsx
+++ src/components/GameShapes/PreBattlePhase.jsx
@@ -6,8 +6,8 @@ export default function PreBattlePhase() {
   const shapesData = useGameData((state) => state.shapes);
   return (
     <>
-      <div>
-        <BgTriangle />
+      <div className="absolute w-full h-full flex justify-center items-center">
+        <BgTriangle className="w-54 h-40" />
       </div>
       {shapesData.map((shape) => (
         <GameShapeItem key={shape.id} shapeData={shape} isComp={false} />
