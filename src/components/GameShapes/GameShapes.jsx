0c3124604b0609e6898cc9138ef8b47478be788e
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-15 07:48:08 +0200

    fasasas

--- src/components/GameShapes/GameShapes.jsx
+++ src/components/GameShapes/GameShapes.jsx
@@ -5,7 +5,7 @@ import PreBattlePhase from "./PreBattlePhase";
 export default function GameShapes() {
   const battleStarted = useGameData((state) => state.battleStarted);
   return (
-    <main className="pt-20 ">
+    <main className="">
       <section className="relative max-w-100 flex flex-wrap justify-center gap-10">
         {battleStarted ? <BattlePhase /> : <PreBattlePhase />}
       </section>
