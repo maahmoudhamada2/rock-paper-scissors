00d227b52765cd73063fdda91984a467c7779ef6
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-15 10:24:37 +0200

    button

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import GameShapes from "./components/GameShapes/GameShapes";
 import Footer from "./components/Footer/Footer";
 export default function App() {
   return (
-    <article className="bg-red-600 w-full h-full min-h-[100vh] p-4 flex flex-col justify-between items-center">
+    <article className="w-full h-full min-h-[100vh] p-4 flex flex-col justify-between items-center">
       <Header />
       <GameShapes />
       <Footer />
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,12 @@ import { useState } from "react";
 export default function Footer() {
   const [showRules, setShowRules] = useState(false);
   return (
-    <footer>
-      <button onClick={() => setShowRules((prev) => !prev)}>RULES</button>
+    <footer className="relative p-4 w-full flex justify-center">
+      <button
+        className="px-6 py-2 text-[0.75rem] text-white font-semibold tracking-wider border-2 border-gray-600 rounded-[9px]"
+        onClick={() => setShowRules((prev) => !prev)}>
+        RULES
+      </button>
       {showRules ? (
         <section>
           <div>
