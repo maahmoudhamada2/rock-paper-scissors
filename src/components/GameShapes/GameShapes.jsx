0be0c7259c3733793c2cef6c7a4b6ebdaacb3884
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-19 08:28:52 +0200

    asasas

--- src/components/GameShapes/GameShapes.jsx
+++ src/components/GameShapes/GameShapes.jsx
@@ -6,7 +6,7 @@ export default function GameShapes() {
   const battleStarted = useGameData((state) => state.battleStarted);
   return (
     <main className="flex-1 flex items-center">
-      <section className="relative max-w-[20rem] flex flex-wrap justify-center gap-[3rem]">
+      <section className="relative max-w-[20rem] flex flex-wrap justify-center gap-x-[3rem] gap-y-[2rem]">
         {battleStarted ? <BattlePhase /> : <PreBattlePhase />}
       </section>
     </main>
