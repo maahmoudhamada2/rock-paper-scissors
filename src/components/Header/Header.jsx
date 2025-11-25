1bc270912768130fa76345679db5d13143090dcb
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-25 15:28:40 +0200

    asasas

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import Score from "./Score";
 export default function Header() {
   return (
     <header className="w-full flex justify-between items-center border-3 border-gray-600 rounded-lg px-[0.7rem] py-2">
-      <Logo className="max-w-[10rem] " />
+      <Logo className="xs:max-w-[10rem] w-[20rem]" />
       <Score />
     </header>
   );
--- src/components/Header/Score.jsx
+++ src/components/Header/Score.jsx
@@ -4,9 +4,13 @@ export default function Score() {
   const score = useGameData((state) => state.score);
 
   return (
-    <section className="bg-white flex flex-col items-center justify-end rounded-lg aspect-2/1 px-[1.5rem] py-[0.7rem] leading-[1.2]">
-      <h1 className="text-[0.6rem] text-blue-700 font-bold">SCORE</h1>
-      <p className="text-[2.1rem] text-navy-900 font-bold">{score}</p>
+    <section className="bg-white flex flex-col items-center justify-end rounded-lg aspect-2/1 px-[1.5rem] py-[0.7rem] sm:px-[2rem] sm:py-[0.8rem] leading-[1.2]">
+      <h1 className="text-[0.6rem] sm:text-[0.8rem] text-blue-700 font-bold">
+        SCORE
+      </h1>
+      <p className="text-[2.1rem] sm:text-[3.2rem] text-navy-900 font-bold">
+        {score}
+      </p>
     </section>
   );
 }
