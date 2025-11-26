31bd94b53983c671e44731b0a273ebed7afc90ee
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-26 17:28:45 +0200

    asas

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
 
   return (
     <>
-      <article className="relative h-screen min-h-screen flex flex-col items-center p-4">
+      <article className="relative min-h-screen flex flex-col items-center p-4">
         <Header />
         <GameShapes />
         <Footer onLoadRules={updateShowRules} />
