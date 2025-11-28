cfacd38e77638fef3e2280e5453fd9c9a4f9101c
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-11-28 17:39:37 +0200

    asasas

--- src/components/GameShapes/BattlePhase.jsx
+++ src/components/GameShapes/BattlePhase.jsx
@@ -16,24 +16,24 @@ export default function BattlePhase() {
   }, []);
 
   return (
-    <div className="w-full h-[70%] pt-[clamp(1rem,6vh,5rem)] grid grid-cols-4 self-center  gap-x-[2rem] gap-y-[2rem] justify-items-center">
+    <div className="w-full h-[80%] grid grid-cols-4 justify-items-center gap-y-[5vh]">
       <section className="w-[clamp(6rem,32vw,9rem)] flex flex-col items-center gap-[1rem] aspect-square row-start-1 row-end-1 col-start-1 col-span-2 ">
         <GameShapeItem shapeData={playerChoice} />
-        <p className="font-bold text-white text-[clamp(0.8rem,3vw,1.3rem)]">
+        <p className="font-bold text-white text-[clamp(0.8rem,2vw,1.3rem)]">
           YOU PICKED
         </p>
       </section>
-      <section className="w-[clamp(6rem,32vw,9rem)] flex flex-col items-center gap-[1rem] aspect-square row-start-1 row-end-1 col-start-3 col-span-2">
+      <section className="w-[clamp(6rem,32vw,9rem)] flex flex-col items-center gap-[1rem] aspect-square row-start-1 row-end-1 col-start-3 col-span-2 ">
         {showResult ? (
           <GameShapeItem shapeData={computerChoice} />
         ) : (
           <div className="w-full aspect-square rounded-full bg-[#182844] "></div>
         )}
-        <p className="font-bold text-white text-[clamp(0.8rem,3vw,1.3rem)]">
+        <p className="font-bold text-white text-[clamp(0.8rem,2vw,1.3rem)]">
           HOUSE PICKED
         </p>
       </section>
-      <section className="flex flex-col justify-center items-center gap-[1.1vh] row-start-2 row-end-2 col-start-1 col-span-4">
+      <section className="flex flex-col justify-center items-center gap-[1.1vh] row-start-2 row-end-2 col-start-1 col-span-4 self-center">
         {showResult && <ResultStatus />}
       </section>
     </div>
