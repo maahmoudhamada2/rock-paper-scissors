992abe404df8fd6c2e6054bb9713c4ba85220a7b
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-12-03 07:15:42 +0200

    asasas

--- src/components/GameShapes/ResultStatus.jsx
+++ src/components/GameShapes/ResultStatus.jsx
@@ -1,16 +1,15 @@
 import useGameData from "../../store/useGameData";
-import clsx from "clsx";
 
 export default function ResultStatus() {
   const resultStatus = useGameData((state) => state.resultStatus);
   const resetBattlePhase = useGameData((state) => state.resetBattlePhase);
   return (
     <>
-      <p className="text-white font-bold text-[clamp(2rem,5vw,2.5rem)] tracking-[0.3rem]">
+      <p className="text-white font-bold text-[clamp(2rem,5vw,2.5rem)] ">
         {resultStatus}
       </p>
       <button
-        className="bg-white py-[0.5rem] px-[clamp(1.2rem,15vw,3.5rem)] rounded-lg"
+        className="bg-white py-[0.5rem] sm:py-[0.7rem] w-[200px] rounded-lg"
         onClick={resetBattlePhase}>
         PLAY AGAIN
       </button>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Score from "./Score";
 
 export default function Header() {
   return (
-    <header className="md:bg-green-500 w-full max-w-[35rem] flex items-center justify-between border-3 border-gray-600 rounded-lg p-2 sm:p-4">
+    <header className="w-full max-w-[35rem] flex items-center justify-between border-3 border-gray-600 rounded-lg p-2 sm:p-4">
       <Logo className="max-w-[20rem] h-[5rem]" />
       <Score />
     </header>
