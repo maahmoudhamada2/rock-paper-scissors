35694f9c75a7fe63891b3d3e273ee3ea6af2df9a
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-12-04 03:28:40 +0200

    asasas

--- src/components/GameShapes/GameShapes.jsx
+++ src/components/GameShapes/GameShapes.jsx
@@ -6,7 +6,7 @@ export default function GameShapes() {
   const battleStarted = useGameData((state) => state.battleStarted);
 
   return (
-    <main className="flex-1 flex items-center">
+    <main className="flex-1 flex items-center justify-center">
       {battleStarted ? <BattlePhase /> : <PreBattlePhase />}
     </main>
   );
