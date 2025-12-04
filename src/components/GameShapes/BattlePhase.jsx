99415fa2b04f2513fc93765e2041497aa2bd6bf2
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-12-04 09:27:30 +0200

    asas

--- src/components/GameShapes/BattlePhase.jsx
+++ src/components/GameShapes/BattlePhase.jsx
@@ -16,24 +16,20 @@ export default function BattlePhase() {
   }, []);
 
   return (
-    <div className="flex flex-wrap">
+    <div className="self-start text-white w-full flex flex-wrap justify-between p-4">
       <section className="w-[clamp(2rem,35vw,6rem)] flex flex-col items-center gap-4">
         <GameShapeItem shapeData={playerChoice} />
-        <p className="font-bold text-white text-[clamp(0.7rem,2vw,1rem)]">
-          YOU PICKED
-        </p>
+        <p className="font-bold text-[clamp(0.7rem,2vw,1rem)]">YOU PICKED</p>
       </section>
       <section className="w-[clamp(2rem,35vw,6rem)] flex flex-col items-center gap-4">
         {showResult ? (
           <GameShapeItem shapeData={computerChoice} />
         ) : (
-          <div></div>
+          <div className="w-full h-full aspect-square rounded-full bg-yellow-200"></div>
         )}
-        <p className="font-bold text-white text-[clamp(0.7rem,2vw,1rem)]">
-          HOUSE PICKED
-        </p>
+        <p className="font-bold  text-[clamp(0.7rem,2vw,1rem)]">HOUSE PICKED</p>
       </section>
-      <section className="w-full flex flex-col items-center">
+      <section className="w-full flex flex-col items-center gap-4">
         {showResult && <ResultStatus />}
       </section>
     </div>
--- src/components/GameShapes/ResultStatus.jsx
+++ src/components/GameShapes/ResultStatus.jsx
@@ -5,8 +5,12 @@ export default function ResultStatus() {
   const resetBattlePhase = useGameData((state) => state.resetBattlePhase);
   return (
     <>
-      <p>{resultStatus}</p>
-      <button onClick={resetBattlePhase}>PLAY AGAIN</button>
+      <p className="font-bold text-[clamp(1.5rem,5vw,3rem)]">{resultStatus}</p>
+      <button
+        className="bg-white rounded-lg text-black px-[1rem] py-[0.3rem]"
+        onClick={resetBattlePhase}>
+        PLAY AGAIN
+      </button>
     </>
   );
 }
