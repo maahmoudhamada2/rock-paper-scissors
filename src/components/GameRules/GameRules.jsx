d79803dc87204bc1d8a203442963582ead9c1f82
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-12-04 09:32:29 +0200

    testing

--- src/components/GameRules/GameRules.jsx
+++ src/components/GameRules/GameRules.jsx
@@ -2,7 +2,7 @@ import { GameRulesImg, CloseIcon } from "../../icons/index";
 
 export default function GameRules({ onLoadRules }) {
   return (
-    <div>
+    <div className="w-full h-full absolute top-0 left-0 bg-white">
       <article>
         <header>
           <h1>RULES</h1>
