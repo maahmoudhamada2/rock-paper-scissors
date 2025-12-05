28a3cdd2664a722d9c2f9712dfad4ec5f30fc934
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-12-05 03:30:43 +0200

    testing

--- src/components/GameShapes/ResultStatus.jsx
+++ src/components/GameShapes/ResultStatus.jsx
@@ -5,9 +5,9 @@ export default function ResultStatus() {
   const resetBattlePhase = useGameData((state) => state.resetBattlePhase);
   return (
     <>
-      <p className="font-bold text-[clamp(2rem,8vw,4rem)]">{resultStatus}</p>
+      <p className="font-bold text-[clamp(2rem,10vw,4rem)]">{resultStatus}</p>
       <button
-        className="bg-white rounded-lg text-black px-[clamp(1rem,12vw,4rem)] py-[0.5rem] sm:py-[0.7rem]"
+        className="bg-white rounded-lg text-black px-[clamp(1rem,15vw,4rem)] py-[0.5rem] sm:py-[0.7rem]"
         onClick={resetBattlePhase}>
         PLAY AGAIN
       </button>
