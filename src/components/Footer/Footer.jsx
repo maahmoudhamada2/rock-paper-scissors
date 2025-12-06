870daf4249b649a5e78d5080c0fcedff26400b75
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-12-07 01:46:32 +0200

    testing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer({ onLoadRules }) {
   return (
-    <footer className="w-full flex justify-center md:justify-end">
+    <footer className="w-full flex justify-center md:justify-end md:py-[1rem]">
       <button
         className="text-[0.9rem] border px-5 py-2 rounded-lg border-white text-white"
         onClick={onLoadRules}>
