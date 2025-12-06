ff04e5d29ee972503c8eb65ac6a415c03b2c09f4
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-12-06 03:08:44 +0200

    testing

--- src/components/GameRules/GameRules.jsx
+++ src/components/GameRules/GameRules.jsx
@@ -2,15 +2,15 @@ import { GameRulesImg, CloseIcon } from "../../icons/index";
 
 export default function GameRules({ onLoadRules }) {
   return (
-    <div className="w-full h-full absolute top-0 left-0">
-      <article className="w-full h-full overflow-auto p-8 flex flex-col justify-around items-center bg-white">
+    <div className="w-full h-full flex justify-center items-center absolute top-0 left-0 md:bg-[#00000091]">
+      <article className="w-full h-full p-8 flex flex-col justify-evenly items-center bg-white md:max-w-[18rem] md:h-[60%] md:rounded-2xl">
         <header>
-          <h1 className="font-bold text-navy-900 text-[clamp(1rem,10vw,1.5rem)]">
+          <h1 className="font-bold text-navy-900 text-[clamp(1rem,10vw,2rem)]">
             RULES
           </h1>
         </header>
         <main>
-          <GameRulesImg className="w-full" />
+          <GameRulesImg className="w-full md:max-w-[15rem]" />
         </main>
         <footer>
           <button onClick={onLoadRules}>
