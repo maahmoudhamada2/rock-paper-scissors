1466978f21fc48933e351a2c907e665999425083
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-12-07 06:53:19 +0200

    testing

--- src/components/GameRules/GameRules.jsx
+++ src/components/GameRules/GameRules.jsx
@@ -2,8 +2,8 @@ import { GameRulesImg, CloseIcon } from "../../icons/index";
 
 export default function GameRules({ onLoadRules }) {
   return (
-    <div className="z-3 w-full h-full min-h-screen py-[20rem] bg-white flex justify-center items-center absolute top-0 left-0 md:bg-[#00000091]">
-      <article className="min-h-[25rem] p-8 flex flex-col justify-around items-center   bg-white md:max-w-[18rem] md:h-[60%] md:rounded-2xl">
+    <div className="z-3 w-full h-full min-h-screen py-[2rem] bg-white flex justify-center items-center absolute top-0 left-0 md:bg-[#00000091]">
+      <article className="max-md:h-full md:min-h-[25rem] p-8 flex flex-col justify-around items-center   bg-white md:max-w-[20rem]  md:rounded-2xl">
         <header>
           <h1 className="font-bold text-navy-900 text-[clamp(1rem,10vw,2rem)]">
             RULES
