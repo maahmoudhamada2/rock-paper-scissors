09105ec87cb56e8e0f94103199c1f14dde0c10f3
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-12-07 10:25:12 +0200

    testing

--- src/ChoosingVersion.jsx
+++ src/ChoosingVersion.jsx
@@ -2,20 +2,23 @@ import classic from "./assets/GameContainer.png";
 import RPSLS from "./assets/RPSLSContainer.png";
 export default function ChoosingVersion() {
   return (
-    <article className="h-full flex flex-col justify-center gap-15 whitespace-nowrap">
+    <article className="pt-[2rem] flex flex-col justify-center gap-10 whitespace-nowrap">
       <header className="flex justify-center font-bold text-white">
-        <h1 className="text-[3rem]">Choose Version</h1>
+        <h1 className="text-[2rem]">Choose Version</h1>
       </header>
-      <main className="w-full flex justify-around items-center">
+      <main className="w-full flex flex-col justify-center items-center gap-8">
         <div className="p-4 rounded-2xl border-2 border-gray-600">
           <button className="text-white font-bold flex flex-col gap-6">
-            <img className="w-[120px] aspect-square" src={classic} />
+            <img className="w-full max-w-[10rem] aspect-square" src={classic} />
             Classic Version
           </button>
         </div>
         <div className="p-4 rounded-2xl border-2 border-gray-600">
           <button className="text-white font-bold flex flex-col gap-6">
-            <img className="w-[120px] aspect-square h-full" src={RPSLS} />
+            <img
+              className="w-full max-w-[10rem] aspect-square h-full"
+              src={RPSLS}
+            />
             RPSLS Version
           </button>
         </div>
