2c8b80797a45b4d4a0fa4695fb14b2b1ec650ad7
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-12-12 08:49:11 +0200

    testing

--- src/GameEdition/GameEdition.jsx
+++ src/GameEdition/GameEdition.jsx
@@ -4,10 +4,10 @@ import ModeCard from "./ModeCard";
 export default function GameEdition() {
   return (
     <div className="flex flex-col items-center gap-[2rem] pt-[2rem]">
-      <h1 className="text-[clamp(2.9rem,10vw,3rem)] text-[#38BDF8] font-[Bangers] leading-none">
+      <h1 className="text-[clamp(2.9rem,10vw,7rem)] text-[#38BDF8] font-[Bangers] leading-none">
         Classic or Chaos
       </h1>
-      <div className="flex flex-col gap-[3rem]">
+      <div className="flex flex-col gap-[3rem] md:flex-row">
         <ModeCard Cover={ClassicBtns} id="classic" />
         <ModeCard Cover={RPSLSBtns} id="rpsls" />
       </div>
