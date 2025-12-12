ea55b9285f3c960ac45e4bb375179a7149a0b1e6
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-12-12 08:50:30 +0200

    testing

--- src/components/GameShapes/GameShapes.jsx
+++ src/components/GameShapes/GameShapes.jsx
@@ -6,7 +6,7 @@ export default function GameShapes() {
   const battleStarted = useGameData((state) => state.battleStarted);
 
   return (
-    <main className="w-full flex-1 flex justify-center items-center ">
+    <main className="w-full flex-1 flex justify-center items-center md:p-4 ">
       {battleStarted ? <BattlePhase /> : <PreBattlePhase />}
     </main>
   );
