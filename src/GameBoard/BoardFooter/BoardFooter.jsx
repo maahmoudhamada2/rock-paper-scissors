9565a6312e86406fadf72c038fa6fa496bc5285e
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-12-15 13:29:49 +0200

    feat: addgame footer for rules

---
+++ src/GameBoard/BoardFooter/BoardFooter.jsx
@@ -0,0 +1,11 @@
+export default function BoardFooter({ onLoadRules }) {
+  return (
+    <footer className="w-full flex justify-center gap-2 md:justify-end md:py-2">
+      <button
+        className="cursor-pointer text-[0.9rem] border px-5 py-2 rounded-lg border-white text-white"
+        onClick={onLoadRules}>
+        RULES
+      </button>
+    </footer>
+  );
+}
--- src/GameBoard/GameBoard.jsx
+++ src/GameBoard/GameBoard.jsx
@@ -1,11 +1,13 @@
 import GameButtons from "./GameButtons/GameButtons";
 import BoardHeader from "./BoardHeader/BoardHeader";
+import BoardFooter from "./BoardFooter/BoardFooter";
 
 export default function GameBoard() {
   return (
     <article className="overflow-y-auto overflow-x-hidden h-full flex flex-col items-center p-4">
       <BoardHeader />
       <GameButtons />
+      <BoardFooter />
     </article>
   );
 }
